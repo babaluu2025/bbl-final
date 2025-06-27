c2a08953a6577cd5ff2949028da86d53b5e9b4fc
babaluu2025 <poratfaktura@gmail.com>
2025-06-27 16:08:20 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
+  storageBucket: "YOUR_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
