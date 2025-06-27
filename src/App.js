1861e264e5ee6ce119e8b6a34891729f7047005f
babaluu2025 <poratfaktura@gmail.com>
2025-06-27 14:52:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import DayEntry from "./DayEntry";          // Glavna komponenta za unos dana
-import SummaryView from "./SummaryView";    // Nova komponenta za sumarno
+import DayEntry from "./DayEntry";
+import SummaryView from "./SummaryView";
 
 function App() {
   return (
     <Router>
-      <div style={{ padding: "20px", maxWidth: "800px", margin: "0 auto" }}>
-        <h1>📘 BBL Billing App</h1>
+      <div style={{ padding: 20 }}>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/" style={{ marginRight: 15 }}>➕ Unos dana</Link>
+          <Link to="/pregled">📂 Sumarni pregled</Link>
+        </nav>
 
-        {/* Navigaciona dugmad */}
-        <div style={{ marginBottom: "20px" }}>
-          <Link to="/">
-            <button style={{ marginRight: "10px" }}>📝 Unos dana</button>
-          </Link>
-          <Link to="/summary">
-            <button>📂 Sumarni pregled</button>
-          </Link>
-        </div>
-
-        {/* Rute */}
         <Routes>
           <Route path="/" element={<DayEntry />} />
-          <Route path="/summary" element={<SummaryView />} />
+          <Route path="/pregled" element={<SummaryView />} />
         </Routes>
       </div>
     </Router>
