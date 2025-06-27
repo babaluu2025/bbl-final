f23864e77ed0becae08f7b2fa47e5b0c6aba4ba3
babaluu2025 <poratfaktura@gmail.com>
2025-06-27 14:55:14 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtmDU5nuhq8qggBZSQ_GVW1c3rnL6H0_8",
-  authDomain: "bbl-billing-app.firebaseapp.com",
-  projectId: "bbl-billing-app",
-  storageBucket: "bbl-billing-app.firebasestorage.app",
-  messagingSenderId: "879061229936",
-  appId: "1:879061229936:web:e44afe2366b704b3862fb5"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
