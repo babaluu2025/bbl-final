c392e64865e28ab87a58f1f3d86ae135803fbc1b
babaluu2025 <poratfaktura@gmail.com>
2025-10-11 19:20:33 +0200

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+// public/service-worker.js
+const CACHE_NAME = 'bbl-billing-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
