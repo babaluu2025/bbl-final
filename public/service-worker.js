efd6f1832901e11f51c9363f19cb8253b63bed56
babaluu2025 <poratfaktura@gmail.com>
2025-10-12 19:12:40 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,51 +1,52 @@
 const CACHE_NAME = 'bbl-billing-v3';
 const urlsToCache = [
-  '/',
-  '/static/js/bundle.js',
-  '/static/css/main.css',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/index.html'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
+// ✅ INSTALL
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing...');
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app shell...');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// ✅ ACTIVATE
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activating...');
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
 
+// ✅ FETCH
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match('./index.html')
+        )
+      );
+    })
   );
 });
